6c2b1286b21e04d2c1cd3bda0e2ba4280b961382
BrodyDubach <117411713+BrodyDubach@users.noreply.github.com>
2023-10-21 01:39:57 +0000

    Finished.

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@ import "../style/index.css";
  *  This function is called every time the user changes types or changes any input
  * 
     {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        includeCover: true, // if includeCover is true the algorithm should
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
+        socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
         github: null,
@@ -16,14 +16,14 @@ import "../style/index.css";
         instagram: null,
 
         name: null,
-        lastName: null,
+        lastname: null,
         role: null,
         country: null,
         city: null
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
+  console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.firstname} ${variables.middlename} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="https://twitter.com/4geeksacademy"><span class="fab fa-twitter"></span></a></li>
+          <li><a href="https://github.com/4geeksacademy"><span class="fab fa-github"></span></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><span class="fab fa-linkedin"></span></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><span class="fab fa-instagram"></span></a></li>
           </ul>
         </div>
     `;
@@ -51,26 +51,29 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
+    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    // this is the url of the image that will used as background for the profile cover
+    background:
+      "https://static.wikia.nocookie.net/spaceships/images/9/9c/Planetx.jpeg/revision/latest?cb=20160223175310",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    github: "https://github.com/BrodyDubach",
+    linkedin: "https://linkedin.com/4geeksacademy",
+    instagram: "https://instagram.com/4geeksacademy",
+    firstname: "Bender",
+    middlename: "Bending",
+    lastname: "Rodriguez",
+    role: "Bending Unit",
+    country: "USA",
+    city: "New New York"
   };
-  render(window.variables); // render the card for the first time
+  render(window.variables); //render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
@@ -85,7 +88,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values)); // render again the card with new valus
     });
   });
 };
